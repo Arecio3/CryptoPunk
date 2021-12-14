198b9dad527bbd23f87595154b80cf4a1083768f
Arecio Canton <devmain@Arecios-iMac.local>
2021-12-14 11:09:23 -0500

    Working on card collection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import CollectionCard from './components/CollectionCard';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className='app'>
       <Header />
+      <CollectionCard id={0} name={'BORED APE'} traits={[{'value': 7}]} image='https://lh3.googleusercontent.com/lxp1GoTos5uWeRXhtFj03i9GxGe9BgPoqeWJy-XstXErNpWa-m334WUO0knPj13SbCOS8AadyRkD4aVRj8CdXYgzD5FqslyyxJCgxaA=w278'/>
     </div>
   );
 }
