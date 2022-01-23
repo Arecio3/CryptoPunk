28af420f27cdb2ef293b6a852e21c91770535d53
Arecio Canton <devmain@Arecios-iMac.local>
2022-01-22 20:35:58 -0500

    successfully getting opensea data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
 import CollectionCard from './components/CollectionCard';
 import Header from './components/Header';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [nftData, setNftData] = useState([])
+
+  useEffect(() => {
+    const getMyNfts = async () => {
+      const openSeaData = await axios.get('https://testnets-api.opensea.io/assets?asset_contract_address=0xd6202790f9fB2a40F7DC4269914B89B4c0Cd5114&order_direction=asc')
+      console.log(openSeaData.data.assets)
+    }
+    return getMyNfts()
+  }, [])
+  
   return (
     <div className='app'>
       <Header />
