02f951b8f5778f0c7a90426c45f4ccb74fcf4b8b
Arecio Canton <devmain@Arecios-iMac.local>
2022-01-22 20:11:57 -0500

    card is done

--- src/components/CollectionCard.js
+++ src/components/CollectionCard.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import weth from '../assets/weth.png'
+import '../styles/CollectionCard.css'
 
 const CollectionCard = ({id, name, traits, image}) => {
     return (
         <div className='collectionCard'>
-            <img src={image} alt="" />
-            <div className="details"></div>
+            <img src={image} alt="" className='NFTImg'/>
+            <div className="details">
             <div className="name">
                 {name}
-                <div className="id">{id}</div>
+                <div className="id">•#{id}</div>
             </div>
             <div className="priceContainer">
                 <img src={weth} className='wethImage' alt=""/>
                 <div className="price">{traits[0]?.value}</div>
+            </div>
             </div>
         </div>
     )
